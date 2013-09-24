3dcd233be6416ccae5a40be0d18341ada432a389
Caleb Briggs <caleb@ninecollective.com>
2013-09-24 10:14:13 -0500

    merging

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ httpProxy.createServer({
     router: {
         'barbstudios.com': 'barbstudios.com:3000',
         'www.barbstudios.com': 'barbstudios.com:3000',
-        'bible.barbstudios.com' : 'barbstudios.com:81',
-        'www.bible.barbstudios.com' : 'barbstudios.com:81'
+        'bible.barbstudios.com' : 'barbstudios.com:3001',
+        'www.bible.barbstudios.com' : 'barbstudios.com:3001'
     }
 }).listen(80);
