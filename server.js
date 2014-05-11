4afd53f723a68b2404990769c724c25f0f6052ea
Caleb Briggs <caleb.briggs@gmail.com>
2014-05-11 09:11:01 -0500

    adding samgenetics pointing to barbstudios for now

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ httpProxy.createServer({
         'barbstudios.com': 'barbstudios.com:3000',
         'www.barbstudios.com': 'barbstudios.com:3000',
         'bible.barbstudios.com' : 'barbstudios.com:3001',
-        'pres.barbstudios.com' : '/var/www/presentation'
-
+        'pres.barbstudios.com' : '/var/www/presentation',
+        'www.samgenetics.com': 'barbstudios.com:3000',
+        'samgenetics.com': 'barbstudios.com:3000'
     }
 }).listen(80);
